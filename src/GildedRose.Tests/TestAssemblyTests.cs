1f9217d9f7d08fc82b27384e83029c590be3feec
Scott Edwards <scotteza@gmail.com>
2019-02-06 09:00:44 +0000

    Add first characterisation test

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,5 @@
+using GildedRose.Console;
+using System.Collections.Generic;
 using Xunit;
 
 namespace GildedRose.Tests
@@ -5,9 +7,20 @@ namespace GildedRose.Tests
     public class TestAssemblyTests
     {
         [Fact]
-        public void TestTheTruth()
+        public void Standard_Item_Degrades_By_1()
         {
-            Assert.True(true);
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "Standard boring item", SellIn = 10, Quality = 10}
+                }
+            };
+
+            app.UpdateQuality();
+
+            Assert.Equal(app.Items[0].Quality, 9);
+            Assert.Equal(app.Items[0].SellIn, 9);
         }
     }
 }
