cf116e8fcfd882868dd55d50018ce1dfc070f33b
Scott Edwards <scotteza@gmail.com>
2019-02-06 09:34:36 +0000

    Add tests aged brie

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -31,10 +31,12 @@ namespace GildedRose.Tests
         public void Quality_Never_Drops_Below_Zero()
         {
             app.Items.Add(StandardItem(sellIn: 0, quality: 0));
+            app.Items.Add(StandardItem(sellIn: -1, quality: 0));
 
             app.UpdateQuality();
 
             Assert.Equal(0, app.Items[0].Quality);
+            Assert.Equal(0, app.Items[1].Quality);
         }
 
         [Fact]
@@ -46,6 +48,43 @@ namespace GildedRose.Tests
 
             Assert.Equal(-1, app.Items[0].SellIn);
 
+        }
+
+        [Fact]
+        public void Quality_Degrades_Twice_As_Fast_After_Sell_By_Date()
+        {
+            app.Items.Add(StandardItem(sellIn: 0, quality: 10));
+
+            app.UpdateQuality();
+
+            Assert.Equal(8, app.Items[0].Quality);
+        }
+
+        [Fact]
+        public void Aged_Brie_Increases_In_Value_As_It_Ages()
+        {
+            app.Items.Add(BrieItem(sellIn: 10, quality: 10));
+
+            app.UpdateQuality();
+
+            Assert.Equal(11, app.Items[0].Quality);
+        }
+
+        [Fact]
+        public void Aged_Brie_Increases_Twice_As_Fast_After_Its_Sell_By_Date()
+        {
+            app.Items.Add(BrieItem(sellIn: 0, quality: 10));
+            app.Items.Add(BrieItem(sellIn: -1, quality: 10));
+
+            app.UpdateQuality();
+
+            Assert.Equal(12, app.Items[0].Quality);
+            Assert.Equal(12, app.Items[1].Quality);
+        }
+
+        private Item BrieItem(int sellIn, int quality)
+        {
+            return new Item { Name = "Aged Brie", SellIn = sellIn, Quality = quality };
         }
 
         private static Item StandardItem(int sellIn, int quality)
