8160bc373626f3cfaa7901f5b8f51b6fc1ba8c55
Scott Edwards <scotteza@gmail.com>
2019-02-07 16:06:13 +0000

    Extract all update behaviour into Item class

---
+++ src/GildedRose.Console/Item.cs
@@ -0,0 +1,99 @@
+﻿namespace GildedRose.Console
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+
+        public bool IsExpired()
+        {
+            return SellIn < 0;
+        }
+
+        public void IncrementQualityWithBoundsCheck()
+        {
+            if (Quality < 50)
+            {
+                Quality = Quality + 1;
+            }
+        }
+
+        public void IncrementConcertTicketQuality()
+        {
+            IncrementQualityWithBoundsCheck();
+
+            if (SellIn < 11)
+            {
+                IncrementQualityWithBoundsCheck();
+            }
+
+            if (SellIn < 6)
+            {
+                IncrementQualityWithBoundsCheck();
+            }
+        }
+
+        public bool IsConcertTicket()
+        {
+            return Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        public bool IsAgedBrie()
+        {
+            return Name == "Aged Brie";
+        }
+
+        public bool IsNotSulfuras()
+        {
+            return Name != "Sulfuras, Hand of Ragnaros";
+        }
+
+        public void UpdateQuality()
+        {
+            if (IsAgedBrie())
+            {
+                IncrementQualityWithBoundsCheck();
+            }
+            else if (IsConcertTicket())
+            {
+                IncrementConcertTicketQuality();
+            }
+            else if (IsNotSulfuras())
+            {
+                DecrementQuality();
+            }
+
+            if (IsNotSulfuras())
+            {
+                SellIn = SellIn - 1;
+            }
+
+            if (IsExpired())
+            {
+                if (IsAgedBrie())
+                {
+                    IncrementQualityWithBoundsCheck();
+                }
+                else if (IsConcertTicket())
+                {
+                    Quality = Quality - Quality;
+                }
+                else if (IsNotSulfuras())
+                {
+                    DecrementQuality();
+                }
+            }
+        }
+
+        private void DecrementQuality()
+        {
+            if (Quality > 0)
+            {
+                Quality = Quality - 1;
+            }
+        }
+    }
+}
